3050d11cd539d8e2f3d17abb41c0f4c699bd3452
Mark F Rodriguez <macrod@gmail.com>
2019-09-25 15:22:33 -0400

    Change initial log level to Info

--- src/lib.rs
+++ src/lib.rs
@@ -66,7 +66,7 @@ pub fn init_with_level(level: Level) -> Result<(), SetLoggerError> {
 /// # }
 /// ```
 pub fn init() -> Result<(), SetLoggerError> {
-    init_with_level(Level::Trace)
+    init_with_level(Level::Info)
 }
 
 #[cfg(test)]
